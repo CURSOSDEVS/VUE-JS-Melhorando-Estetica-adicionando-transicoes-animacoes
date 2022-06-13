d5758f8ce436a55ae55d1942172269989235011e
CURSOSDEVS <claudisneibello@hotmail.com>
2022-06-13 09:51:45 -0300

    Criando transição Fade com CSS

--- animacoes-exercicios/src/App.vue
+++ animacoes-exercicios/src/App.vue
@@ -1,13 +1,24 @@
 <template>
 	<div id="app" class="container-fluid">
 		<h1>Animações</h1>
+		<hr>
+		<b-button variant="primary" class="mb-4"
+			@click="exibir = !exibir">Mostrar Mensagem</b-button>
+		<transition name="fade">
+			<b-alert variant="info" show v-if="exibir">{{msg}}</b-alert>
+		</transition>
 	</div>
 </template>
 
 <script>
 
 export default {
-
+	data() {
+		return {
+			msg: 'Uma mensagem de informação para o usuário!',
+			exibir: false
+		}
+	},
 }
 </script>
 
@@ -21,4 +32,13 @@ export default {
 	margin-top: 60px;
 	font-size: 1.5rem;
 }
+
+.fade-enter, .fade-leave-to{
+	opacity: 0;
+}
+
+.fade-enter-active, .fade-leave-active{
+	transition: opacity 2s;
+}
+
 </style>
